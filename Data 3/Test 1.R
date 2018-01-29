0717a9070050c7c2333e716030afcc76fdbc4937
James Gallacher <35454237+jwgiom@users.noreply.github.com>
2018-01-29 15:44:59 +0000

    Commit befroe merge

--- Data 3/Test 1.R
+++ Data 3/Test 1.R
@@ -14,3 +14,5 @@ Data<-read_tsv("/Users/jamesgallacher/Desktop/R/UKDA-6614-tab/tab/us_w1/a_indres
 Gender<-Data$a_scsex
 Educ<-c(Data$a_nqfhigh_dv,Data$a_qfhighfl_dv,Data$a_qfhigh_dv,Data$a_hiqual_dv)
 Pol<- c(Data$a_vote1,Data$a_vote2,Data$a_vote3,Data$a_vote4,Data$a_vote5,Data$a_vote6)
+
+
